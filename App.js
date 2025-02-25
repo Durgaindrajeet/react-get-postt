22550e0a5d374c6a1a390d1f43a87865b49b1bf6
Durga I <88365944+Durgaindrajeet@users.noreply.github.com>
2025-02-25 09:03:01 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import axios from "axios";
+const App = () => {
+  //get request related to string
+  const getDataFromBackend = async () => {
+    const response = await axios.get("http://localhost:8080/call");
+    console.log(response.data);
+    document.getElementById("para").innerHTML = response.data;
+  };
+  // post request related to string
+  const data = "Hello";
+  const PostDataFromFrontend = async () => {
+    const response = await axios.post("http://localhost:8080/testpost", {
+      data,
+    });
+    console.log(response.data);
+    document.getElementById("para").innerHTML = response.data;
+  };
+
+  //post request related to form
+  const [formData, setFormData] = useState("");
+
+  const postFormFromFrontend = async () => {
+    const response = await axios.post("http://localhost:8080/testform", {
+      formData,
+    });
+    console.log(response.data);
+    document.getElementById("para").innerHTML = response.data;
+  };
+
+  return (
+    <div className="App">
+      <button onClick={PostDataFromFrontend}>Post Data</button>
+      <p id="para"></p>
+      <br />
+
+      <button onClick={getDataFromBackend}>Get Data</button>
+      <p id="para"></p>
+
+      <br />
+      <br />
+      <form onSubmit={postFormFromFrontend}>
+        <input
+          type="text"
+          name="formData"
+          value={formData}
+          onChange={(e) => setFormData(e.target.value)}
+        ></input>
+        <input type="submit" value="Test Form"></input>
+      </form>
+    </div>
+  );
+};
+export default App;
